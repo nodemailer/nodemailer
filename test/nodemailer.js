14579d705032283d438115a1c13f0fda27b21d0e
Andris Reinman <andris@node.ee>
2012-02-20 10:30:34 +0200

    Fixed #70

--- test/nodemailer.js
+++ test/nodemailer.js
@@ -9,3 +9,54 @@ exports["General tests"] = {
         test.done();
     }
 }
+
+exports["Transport close"] = {
+    "SMTP - Callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("SMTP", {});
+        transport.close(function(){
+            test.ok(true);
+            test.done();
+        });
+    },
+    
+    "SMTP - No callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("SMTP", {});
+        transport.close();
+        process.nextTick(function(){
+            test.ok(true);
+            test.done();
+        });
+    },
+    "Sendmail - Callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("Sendmail", {});
+        transport.close(function(){
+            test.ok(true);
+            test.done();
+        });
+    },
+    
+    "Sendmail - No callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("Sendmail", {});
+        transport.close();
+        process.nextTick(function(){
+            test.ok(true);
+            test.done();
+        });
+    },
+    "SES - Callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("SES", {});
+        transport.close(function(){
+            test.ok(true);
+            test.done();
+        });
+    },
+    
+    "SES - No callback in transport.close": function(test){
+        var transport = nodemailer.createTransport("SES", {});
+        transport.close();
+        process.nextTick(function(){
+            test.ok(true);
+            test.done();
+        });
+    }
+}
