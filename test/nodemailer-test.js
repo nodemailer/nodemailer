6111d199a238d67e8ff090817c024753bb6012e5
Andris Reinman <andris@kreata.ee>
2016-01-04 12:12:32 +0200

    Use nodemailer-smtp-pool from the beta channel

--- test/nodemailer-test.js
+++ test/nodemailer-test.js
@@ -227,173 +227,258 @@ describe('Nodemailer integration tests', function () {
         server.close(done);
     });
 
-    it('should log in and send mail', function (done) {
-        var nm = nodemailer.createTransport({
-            host: 'localhost',
-            port: PORT_NUMBER,
-            auth: {
-                user: 'testuser',
-                pass: 'testpass'
-            },
-            ignoreTLS: true,
-            logger: false
+    describe('smtp-transport tests', function () {
+
+        it('should log in and send mail', function (done) {
+            var nm = nodemailer.createTransport({
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false
+            });
+
+            var mailData = {
+                from: 'from@valid.sender',
+                sender: 'sender@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: 'uuu'
+            };
+
+            nm.sendMail(mailData, function (err, info) {
+                expect(err).to.not.exist;
+                expect(info.accepted).to.deep.equal([
+                    'to1@valid.recipient',
+                    'to2@valid.recipient'
+                ]);
+                expect(info.rejected).to.deep.equal([
+                    'to@invalid.recipient'
+                ]);
+                expect(info.messageId).to.equal('abc@def');
+                expect(/538ec1431ce376bc46f11b0f51849beb/i.test(info.response)).to.be.true;
+                done();
+            });
         });
 
-        var mailData = {
-            from: 'from@valid.sender',
-            sender: 'sender@valid.sender',
-            to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
-            subject: 'test',
-            date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
-            messageId: 'abc@def',
-            xMailer: 'aaa',
-            text: 'uuu'
-        };
+        it('should return stream error, not send', function (done) {
+            var nm = nodemailer.createTransport({
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false
+            });
 
-        nm.sendMail(mailData, function (err, info) {
-            expect(err).to.not.exist;
-            expect(info.accepted).to.deep.equal([
-                'to1@valid.recipient',
-                'to2@valid.recipient'
-            ]);
-            expect(info.rejected).to.deep.equal([
-                'to@invalid.recipient'
-            ]);
-            expect(info.messageId).to.equal('abc@def');
-            expect(/538ec1431ce376bc46f11b0f51849beb/i.test(info.response)).to.be.true;
-            done();
+            var mailData = {
+                from: 'from@valid.sender',
+                sender: 'sender@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: new stream.PassThrough()
+            };
+
+            nm.sendMail(mailData, function (err) {
+                expect(err).to.exist;
+                done();
+            });
+
+            mailData.text.write('teretere');
+            setTimeout(function () {
+                mailData.text.emit('error', new Error('Stream error'));
+            }, 400);
         });
+
+        it('should response auth error', function (done) {
+            var nm = nodemailer.createTransport({
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'invalid user',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false
+            });
+
+            var mailData = {
+                from: 'from@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: 'uuu'
+            };
+
+            nm.sendMail(mailData, function (err, info) {
+                expect(err).to.exist;
+                expect(info).to.not.exist;
+                expect(err.code).to.equal('EAUTH');
+                done();
+            });
+        });
+
+        it('should response envelope error', function (done) {
+            var nm = nodemailer.createTransport({
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false
+            });
+
+            var mailData = {
+                from: 'from@valid.sender',
+                to: ['to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: 'uuu'
+            };
+
+            nm.sendMail(mailData, function (err, info) {
+                expect(err).to.exist;
+                expect(info).to.not.exist;
+                expect(err.code).to.equal('EENVELOPE');
+                done();
+            });
+        });
+
+        it('should override envelope', function (done) {
+            var nm = nodemailer.createTransport({
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false
+            });
+
+            var mailData = {
+                from: 'from@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: 'uuu',
+                envelope: {
+                    from: 'aaa@valid.sender',
+                    to: 'vvv@valid.recipient'
+                }
+            };
+
+            nm.sendMail(mailData, function (err, info) {
+                expect(err).to.not.exist;
+                expect(info.accepted).to.deep.equal([
+                    'vvv@valid.recipient'
+                ]);
+                expect(info.rejected).to.deep.equal([]);
+                expect(info.messageId).to.equal('abc@def');
+                expect(/eaa13435e1401328be32bc7a4c629f9f/i.test(info.response)).to.be.true;
+                done();
+            });
+        });
+
     });
 
-    it('should return stream error, not send', function (done) {
-        var nm = nodemailer.createTransport({
-            host: 'localhost',
-            port: PORT_NUMBER,
-            auth: {
-                user: 'testuser',
-                pass: 'testpass'
-            },
-            ignoreTLS: true,
-            logger: false
+    describe('smtp-pool tests', function () {
+        it('should log in and send mail', function (done) {
+            var nm = nodemailer.createTransport({
+                pool: true,
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                logger: false,
+                debug: true
+            });
+
+            var mailData = {
+                from: 'from@valid.sender',
+                sender: 'sender@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: 'uuu'
+            };
+
+            nm.sendMail(mailData, function (err, info) {
+                nm.close();
+                expect(err).to.not.exist;
+                expect(info.accepted).to.deep.equal([
+                    'to1@valid.recipient',
+                    'to2@valid.recipient'
+                ]);
+                expect(info.rejected).to.deep.equal([
+                    'to@invalid.recipient'
+                ]);
+                expect(info.messageId).to.equal('abc@def');
+                expect(/538ec1431ce376bc46f11b0f51849beb/i.test(info.response)).to.be.true;
+                done();
+            });
         });
 
-        var mailData = {
-            from: 'from@valid.sender',
-            sender: 'sender@valid.sender',
-            to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
-            subject: 'test',
-            date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
-            messageId: 'abc@def',
-            xMailer: 'aaa',
-            text: new stream.PassThrough()
-        };
+        it('should return stream error, not send', function (done) {
+            var nm = nodemailer.createTransport({
+                pool: true,
+                host: 'localhost',
+                port: PORT_NUMBER,
+                auth: {
+                    user: 'testuser',
+                    pass: 'testpass'
+                },
+                ignoreTLS: true,
+                maxConnections: 1,
+                logger: false,
+                debug: true
+            });
 
-        nm.sendMail(mailData, function (err) {
-            expect(err).to.exist;
-            done();
-        });
+            var mailData = {
+                from: 'from@valid.sender',
+                sender: 'sender@valid.sender',
+                to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
+                subject: 'test',
+                date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
+                messageId: 'abc@def',
+                xMailer: 'aaa',
+                text: new stream.PassThrough()
+            };
 
-        mailData.text.write('teretere');
-        setTimeout(function () {
-            mailData.text.emit('error', new Error('Stream error'));
-        }, 400);
-    });
+            nm.sendMail(mailData, function (err) {
+                nm.close();
+                expect(err).to.exist;
+                done();
+            });
 
-    it('should response auth error', function (done) {
-        var nm = nodemailer.createTransport({
-            host: 'localhost',
-            port: PORT_NUMBER,
-            auth: {
-                user: 'invalid user',
-                pass: 'testpass'
-            },
-            ignoreTLS: true,
-            logger: false
-        });
-
-        var mailData = {
-            from: 'from@valid.sender',
-            to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
-            subject: 'test',
-            date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
-            messageId: 'abc@def',
-            xMailer: 'aaa',
-            text: 'uuu'
-        };
-
-        nm.sendMail(mailData, function (err, info) {
-            expect(err).to.exist;
-            expect(info).to.not.exist;
-            expect(err.code).to.equal('EAUTH');
-            done();
-        });
-    });
-
-    it('should response envelope error', function (done) {
-        var nm = nodemailer.createTransport({
-            host: 'localhost',
-            port: PORT_NUMBER,
-            auth: {
-                user: 'testuser',
-                pass: 'testpass'
-            },
-            ignoreTLS: true,
-            logger: false
-        });
-
-        var mailData = {
-            from: 'from@valid.sender',
-            to: ['to@invalid.recipient'],
-            subject: 'test',
-            date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
-            messageId: 'abc@def',
-            xMailer: 'aaa',
-            text: 'uuu'
-        };
-
-        nm.sendMail(mailData, function (err, info) {
-            expect(err).to.exist;
-            expect(info).to.not.exist;
-            expect(err.code).to.equal('EENVELOPE');
-            done();
-        });
-    });
-
-    it('should override envelope', function (done) {
-        var nm = nodemailer.createTransport({
-            host: 'localhost',
-            port: PORT_NUMBER,
-            auth: {
-                user: 'testuser',
-                pass: 'testpass'
-            },
-            ignoreTLS: true,
-            logger: false
-        });
-
-        var mailData = {
-            from: 'from@valid.sender',
-            to: ['to1@valid.recipient', 'to2@valid.recipient', 'to@invalid.recipient'],
-            subject: 'test',
-            date: new Date('Mon, 31 Jan 2011 23:01:00 +0000'),
-            messageId: 'abc@def',
-            xMailer: 'aaa',
-            text: 'uuu',
-            envelope: {
-                from: 'aaa@valid.sender',
-                to: 'vvv@valid.recipient'
-            }
-        };
-
-        nm.sendMail(mailData, function (err, info) {
-            expect(err).to.not.exist;
-            expect(info.accepted).to.deep.equal([
-                'vvv@valid.recipient'
-            ]);
-            expect(info.rejected).to.deep.equal([]);
-            expect(info.messageId).to.equal('abc@def');
-            expect(/eaa13435e1401328be32bc7a4c629f9f/i.test(info.response)).to.be.true;
-            done();
+            mailData.text.write('teretere');
+            setTimeout(function () {
+                mailData.text.emit('error', new Error('Stream error'));
+            }, 400);
         });
     });
 });
