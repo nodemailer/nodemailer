1ceab54918107013ee8e81067f81c66917444e24
Andris Reinman <andris@kreata.ee>
2017-02-09 23:15:42 +0200

    Added some missing tests for icalEvent

--- test/json-transport/json-transport-test.js
+++ test/json-transport/json-transport-test.js
@@ -72,4 +72,65 @@ describe('JSON Transport Tests', function () {
 
     });
 
+    it('should return an JSON string for calendar event', function (done) {
+
+        let transport = nodemailer.createTransport({
+            jsonTransport: true
+        });
+
+        let messageObject = {
+            from: 'Andris Reinman <andris.reinman@gmail.com>',
+            to: 'Andris Kreata <andris@kreata.ee>, andris@nodemailer.com',
+            cc: 'info@nodemailer.com',
+            subject: 'Awesome!',
+            messageId: '<fede478a-aab9-af02-789c-ad93a76a3548@gmail.com>',
+            html: '<p>hello world!</p>',
+            text: 'hello world',
+            icalEvent: {
+                method: 'request',
+                path: __dirname + '/fixtures/event.ics'
+            }
+        };
+
+        transport.sendMail(messageObject, (err, info) => {
+            expect(err).to.not.exist;
+            expect(info).to.exist;
+            expect(JSON.parse(info.message)).to.deep.equal({
+                from: {
+                    address: 'andris.reinman@gmail.com',
+                    name: 'Andris Reinman'
+                },
+                to: [
+                    //
+                    {
+                        address: 'andris@kreata.ee',
+                        name: 'Andris Kreata'
+                    },
+                    {
+                        address: 'andris@nodemailer.com',
+                        name: ''
+                    }
+                ],
+                cc: [{
+                    address: 'info@nodemailer.com',
+                    name: ''
+                }],
+                subject: 'Awesome!',
+                text: 'hello world',
+
+                html: '<p>hello world!</p>',
+                icalEvent: {
+                    content: 'QkVHSU46VkNBTEVOREFSClZFUlNJT046Mi4wClBST0RJRDotLy9oYWNrc3cvaGFuZGNhbC8vTk9OU0dNTCB2MS4wLy9FTgpCRUdJTjpWRVZFTlQKVUlEOnVpZDFAZXhhbXBsZS5jb20KRFRTVEFNUDoxOTk3MDcxNFQxNzAwMDBaCk9SR0FOSVpFUjtDTj1Kb2huIERvZTpNQUlMVE86am9obi5kb2VAZXhhbXBsZS5jb20KRFRTVEFSVDoxOTk3MDcxNFQxNzAwMDBaCkRURU5EOjE5OTcwNzE1VDAzNTk1OVoKU1VNTUFSWTpCYXN0aWxsZSBEYXkgUGFydHkKRU5EOlZFVkVOVApFTkQ6VkNBTEVOREFSCg==',
+                    encoding: 'base64',
+                    method: 'request'
+                },
+
+                headers: {},
+                messageId: '<fede478a-aab9-af02-789c-ad93a76a3548@gmail.com>'
+            });
+            done();
+        });
+
+    });
+
 });
