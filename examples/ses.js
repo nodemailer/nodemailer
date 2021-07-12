cc4601c4d95c5bbe74a02afd60c53ad55483c97e
Andris Reinman <andris@kreata.ee>
2021-07-12 09:40:24 +0300

    Updated SES example

--- examples/ses.js
+++ examples/ses.js
@@ -1,10 +1,23 @@
 'use strict';
 
 let nodemailer = require('nodemailer');
-let aws = require('@aws-sdk/client-ses');
 
-process.env.AWS_ACCESS_KEY_ID = '....';
-process.env.AWS_SECRET_ACCESS_KEY = '....';
+/* --- Select AWS SDK version by uncommenting correct version --- */
+
+let aws = require('@aws-sdk/client-ses');
+//let aws = require('aws-sdk');
+
+/* --- Change these values to test --- */
+
+const AWS_ACCESS_KEY_ID = '...';
+const AWS_SECRET_ACCESS_KEY = '...';
+const FROM_ADDRESS = 'andris.reinman@gmail.com';
+const TO_ADDRESS = 'andris@kreata.ee';
+
+/* --- no need to change below this line when testing --- */
+
+process.env.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID;
+process.env.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY;
 
 const ses = new aws.SES({
     apiVersion: '2010-12-01',
@@ -19,10 +32,11 @@ let transporter = nodemailer.createTransport({
 // send some mail
 transporter.sendMail(
     {
-        from: 'andris.reinman@gmail.com',
-        to: 'andris@kreata.ee',
-        subject: 'Message',
-        text: 'I hope this message gets sent!',
+        from: FROM_ADDRESS,
+        to: TO_ADDRESS,
+
+        subject: 'Message ✓ ' + Date.now(),
+        text: 'I hope this message gets sent! ✓',
         ses: {
             // optional extra arguments for SendRawEmail
             Tags: [
